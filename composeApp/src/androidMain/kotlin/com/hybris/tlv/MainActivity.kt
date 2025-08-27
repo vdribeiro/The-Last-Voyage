820604a77b42e904b4a8b911ed0925542014a783
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-08-27 17:16:18 +0100

    Improve navigation

--- composeApp/src/androidMain/kotlin/com/hybris/tlv/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/hybris/tlv/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.backhandler.BackHandler
 import app.cash.sqldelight.db.SqlDriver
 import com.hybris.tlv.database.createSqlDriver
 import com.hybris.tlv.flow.Dispatcher
@@ -57,13 +55,9 @@ private val navigation: NavigationManager by lazy {
 
 class MainActivity: ComponentActivity() {
 
-    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            BackHandler(enabled = true) { navigation.back() }
-            App(navigation = navigation)
-        }
+        setContent { App(navigation = navigation) }
     }
 }
--- composeApp/src/desktopMain/kotlin/com/hybris/tlv/main.kt
+++ composeApp/src/desktopMain/kotlin/com/hybris/tlv/main.kt
@@ -1,7 +1,14 @@
 package com.hybris.tlv
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.isBackPressed
+import androidx.compose.ui.input.pointer.isSecondaryPressed
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.WindowState
@@ -63,6 +70,7 @@ private val isJfxInitialized = AtomicBoolean(false)
 
 val LocalWindowState = staticCompositionLocalOf<WindowState?> { null }
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() = application {
     val windowState = rememberWindowState(placement = WindowPlacement.Maximized)
     if (!isJfxInitialized.getAndSet(true)) Platform.startup {}
@@ -73,7 +81,14 @@ fun main() = application {
             state = windowState,
             title = getTranslation(key = "app_name")
         ) {
-            App(navigation = navigation)
+            Box(
+                modifier = Modifier
+                    .onPointerEvent(eventType = PointerEventType.Press) { pointerEvent ->
+                        if (pointerEvent.buttons.isSecondaryPressed || pointerEvent.buttons.isBackPressed) navigation.back()
+                    }
+            ) {
+                App(navigation = navigation)
+            }
         }
     }
 }
