d5d6e6b3f18e5b1ff9cc7122cad2f24e0e89a41a
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-09-08 10:19:31 +0100

    Changed music tracks

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/media/Tracks.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/media/Tracks.kt
@@ -4,6 +4,7 @@ import com.hybris.tlv.ui.navigation.NavigationManager.Screen
 
 internal fun getTracks(screen: Screen) = when (screen) {
     Screen.SPLASH,
+    Screen.FEEDBACK,
     Screen.MAIN_MENU,
     Screen.NEW_GAME,
     Screen.STELLAR_EXPLORER,
@@ -16,21 +17,14 @@ internal fun getTracks(screen: Screen) = when (screen) {
     Screen.GAME,
     Screen.EVENT -> arrayOf(
         "tracks/blind_shift.mp3",
-        "tracks/deep_sea.mp3",
-        "tracks/deep_space_flight.mp3",
-        "tracks/doubts.mp3",
-        "tracks/fields_theme.mp3",
         "tracks/graduality.mp3",
-        "tracks/led_twlight.mp3",
+        "tracks/led_twilight.mp3",
         "tracks/my_very_own_dead_ship.mp3",
         "tracks/neon_sky.mp3",
-        "tracks/observing_the_star.mp3",
-        "tracks/out_there.mp3",
         "tracks/rain_in_space.mp3",
         "tracks/space_gras.mp3",
     )
 
-    Screen.FEEDBACK,
     Screen.GAME_OVER -> arrayOf(
         "tracks/space.mp3",
     )
