01d8f3692507159d75616afc3e384635f9737e77
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-09-30 01:30:03 +0100

    Sentry

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/telemetry/Logger.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/telemetry/Logger.kt
@@ -1,8 +1,17 @@
 package com.hybris.tlv.telemetry
 
+/**
+ * Platform-specific logger.
+ */
 internal expect object Logger {
 
+    /**
+     * Logs an informational message.
+     */
     fun info(tag: String, message: String)
 
+    /**
+     * Logs an error message.
+     */
     fun error(tag: String, message: String, throwable: Throwable? = null)
 }
--- composeApp/src/commonMain/kotlin/com/hybris/tlv/telemetry/SentryLogger.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/telemetry/SentryLogger.kt
@@ -3,8 +3,43 @@ package com.hybris.tlv.telemetry
 import io.sentry.kotlin.multiplatform.Sentry
 import io.sentry.kotlin.multiplatform.SentryLevel
 import io.sentry.kotlin.multiplatform.protocol.Breadcrumb
+import io.sentry.kotlin.multiplatform.protocol.SdkVersion
+import io.sentry.kotlin.multiplatform.protocol.User
 
+/**
+ * Sentry logger.
+ */
 internal object SentryLogger {
+
+
+    /**
+     * Initialize Sentry.
+     */
+    internal fun init() {
+        // TODO
+        //Sentry.init { options ->
+        //    options.dsn = BuildConfig.SENTRY_DSN
+        //    options.release = "TLV@${BuildConfig.VERSION_NAME}+${BuildConfig.VERSION_CODE}"
+        //    options.debug = BuildConfig.DEBUG
+        //    options.environment = if (BuildConfig.DEBUG) "development" else "production"
+        //    options.sdk = SdkVersion()
+        //    options.attachViewHierarchy = BuildConfig.DEBUG
+        //    options.sampleRate = 1.0
+        //    options.tracesSampleRate = 0.2
+        //}
+        //
+        //val user = User().apply {
+        //    id = "player123"
+        //    username = "GamingGod"
+        //    email = "player123@example.com"
+        //}
+        //Sentry.setUser(user)
+    }
+
+    /**
+     * Logs an informational message as a breadcrumb in Sentry.
+     * Breadcrumbs are used to record a trail of events that led up to an issue.
+     */
     fun info(tag: String, message: String) {
         Sentry.addBreadcrumb(breadcrumb = Breadcrumb().apply {
             this.level = SentryLevel.INFO
@@ -14,15 +49,17 @@ internal object SentryLogger {
         })
     }
 
+    /**
+     * Logs an error message to Sentry.
+     * If a [throwable] is provided, it captures the exception along with the [tag] and [message],
+     * otherwise it captures the [message] with the [tag].
+     */
     fun error(tag: String, message: String, throwable: Throwable? = null) {
         when {
-            throwable != null -> Sentry.captureException(throwable = throwable) { scope ->
+            throwable == null -> Sentry.captureMessage(message) { scope -> scope.setTag("tag", tag) }
+            else -> Sentry.captureException(throwable = throwable) { scope ->
                 scope.setTag("tag", tag)
                 scope.setExtra("message", message)
-            }
-
-            else -> Sentry.captureMessage(message) { scope ->
-                scope.setTag("tag", tag)
             }
         }
     }
