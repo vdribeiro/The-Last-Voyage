dedb5d202ba6691a59fb9344fcc1150bb2be9c4e
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-09-30 12:20:14 +0100

    Sentry

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/telemetry/SentryLogger.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/telemetry/SentryLogger.kt
@@ -12,22 +12,23 @@ import io.sentry.kotlin.multiplatform.protocol.UserFeedback
  */
 internal object SentryLogger {
 
+    private val available = Property.SENTRY_DSN.isNotBlank()
+
     /**
      * Initialize Sentry.
      */
     internal fun init() {
-        if (Property.SENTRY_DSN.isNotBlank()) {
-            Sentry.init { options ->
-                options.dsn = Property.SENTRY_DSN
-                options.release = "${Property.APP_NAME.lowercase().replace(regex = "\\s+".toRegex(), replacement = "")}@${Property.APP_VERSION}"
+        if (!available) return
+        Sentry.init { options ->
+            options.dsn = Property.SENTRY_DSN
+            options.release = "${Property.APP_NAME.lowercase().replace(regex = "\\s+".toRegex(), replacement = "")}@${Property.APP_VERSION}"
 
-                options.debug = isDebug
-                options.attachViewHierarchy = isDebug
-                options.environment = if (isDebug) "dev" else "prod"
+            options.debug = isDebug
+            options.attachViewHierarchy = isDebug
+            options.environment = if (isDebug) "dev" else "prod"
 
-                options.sampleRate = 1.0
-                options.tracesSampleRate = 0.2
-            }
+            options.sampleRate = 1.0
+            options.tracesSampleRate = 0.2
         }
     }
 
@@ -35,19 +36,22 @@ internal object SentryLogger {
      * Logs an informational message as a breadcrumb in Sentry.
      * Breadcrumbs are used to record a trail of events that led up to an issue.
      */
-    fun info(tag: String, message: String) =
+    fun info(tag: String, message: String) {
+        if (!available) return
         Sentry.addBreadcrumb(breadcrumb = Breadcrumb().apply {
             this.level = SentryLevel.INFO
             this.category = tag
             this.message = message
         })
+    }
 
     /**
      * Logs an error message to Sentry.
      * If a [throwable] is provided, it captures the exception along with the [tag] and [message],
      * otherwise it captures the [message] with the [tag].
      */
-    fun error(tag: String, message: String, throwable: Throwable? = null) =
+    fun error(tag: String, message: String, throwable: Throwable? = null) {
+        if (!available) return
         when {
             throwable == null -> Sentry.captureMessage(message = message) { scope -> scope.setTag(key = "tag", value = tag) }
             else -> Sentry.captureException(throwable = throwable) { scope ->
@@ -55,12 +59,15 @@ internal object SentryLogger {
                 scope.setExtra(key = "message", value = message)
             }
         }
+    }
 
     /**
      * Logs a user feedback to Sentry.
      */
-    fun feedback(message: String) =
+    fun feedback(message: String) {
+        if (!available) return
         Sentry.captureUserFeedback(userFeedback = UserFeedback(sentryId = Sentry.captureMessage(message = message)).apply {
             this.comments = message
         })
+    }
 }
