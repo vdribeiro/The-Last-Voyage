d0de102dd85daeb0b251b4c8edf703ed26ed5678
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-10-07 14:02:14 +0100

    Engines

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/screen/newgame/NewGameStore.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/screen/newgame/NewGameStore.kt
@@ -54,7 +54,7 @@ internal class NewGameStore(
         val shipState = ShipState(
             totalPoints = 20,
             sensorRange = AttributePoint(max = 10, min = 1, interval = 1, initialValue = 3),
-            fuel = AttributePoint(max = 1000, min = 100, interval = 100, initialValue = 700),
+            fuel = AttributePoint(max = 2000, min = 100, interval = 100, initialValue = 700),
             materials = AttributePoint(max = 1000, min = 100, interval = 100, initialValue = 500),
             cryopods = AttributePoint(max = 1000, min = 100, interval = 100, initialValue = 300),
         )
