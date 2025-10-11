695d1c4bcbb89768f319071be7b8c037b9516cc0
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-10-12 00:42:35 +0100

    Intro Content

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/screen/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/screen/splash/SplashScreen.kt
@@ -1,19 +1,20 @@
 package com.hybris.tlv.ui.screen.splash
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.hybris.tlv.ui.preview.getStore
 import com.hybris.tlv.ui.store.Store
 import com.hybris.tlv.ui.theme.AppTheme
@@ -30,7 +31,17 @@ internal fun SplashScreen(store: Store<SplashState, SplashAction>) {
     val loadingTranslation = getTranslation(key = "splash_screen__loading")
 
     Screen(
-        modifier = Modifier.testTag(tag = SPLASH_SCREEN),
+        modifier = Modifier
+            .testTag(tag = SPLASH_SCREEN)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) {
+                when (currentContent) {
+                    Content.SPLASH -> {}
+                    Content.INTRO -> store.send(action = SplashAction.Next)
+                }
+            },
         loading = storeState.loading,
         loadingDelayMillis = 0L,
         loadingProgress = storeState.progress,
@@ -38,23 +49,18 @@ internal fun SplashScreen(store: Store<SplashState, SplashAction>) {
     ) {
         when (currentContent) {
             Content.SPLASH -> {}
-            Content.INTRO -> Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(all = 32.dp)
-                    .clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null
-                    ) { store.send(action = SplashAction.Next) },
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(all = 32.dp),
-                    text = getTranslation(key = "splash_screen__intro"),
-                    textAlign = TextAlign.Center
-                )
+            Content.INTRO -> {
+                var isVisible by remember { mutableStateOf(value = false) }
+                LaunchedEffect(key1 = Unit) { isVisible = true }
+                AnimatedVisibility(
+                    visible = isVisible,
+                    enter = fadeIn(animationSpec = tween(durationMillis = 2000))
+                ) {
+                    Text(
+                        text = getTranslation(key = "splash_screen__intro"),
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
         }
     }
