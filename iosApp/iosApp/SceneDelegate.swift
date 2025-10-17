811fb623f9b8c45b7a9ac24cdd819df51980820b
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-10-17 18:02:37 +0100

    Ios Scene Delegate

---
+++ iosApp/iosApp/SceneDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+import composeApp
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = MainViewControllerKt.MainViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
