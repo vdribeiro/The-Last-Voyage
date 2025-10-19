ebaf231f9fe79710874b72d48a5c1c53b8b36d92
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-10-19 12:15:54 +0100

    Apple Audio Player

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -5,12 +5,6 @@ import AVFoundation
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
-            try AVAudioSession.sharedInstance().setActive(true)
-        } catch {
-            print("Unable to set audio player: \(error)")
-        }
         return true
     }
     
