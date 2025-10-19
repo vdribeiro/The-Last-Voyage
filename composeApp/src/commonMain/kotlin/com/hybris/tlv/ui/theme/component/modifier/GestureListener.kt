cfe901465251d70a57be7bccd0223263027a350c
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-10-19 23:18:54 +0100

    Easter eggs

---
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/theme/component/modifier/GestureListener.kt
@@ -0,0 +1,107 @@
+package com.hybris.tlv.ui.theme.component.modifier
+
+import kotlin.math.abs
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.pointerInput
+
+@Composable
+internal fun Modifier.onGesture(
+    sequence: List<Gesture>,
+    onSequenceComplete: () -> Unit
+): Modifier = composed {
+    val nestedScrollConnection = remember {
+        object: NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                return Offset.Zero
+            }
+        }
+    }
+
+    val progress = remember { mutableStateListOf<Gesture>() }
+    LaunchedEffect(key1 = progress.size) {
+        if (progress.isNotEmpty()) {
+            delay(timeMillis = 2000L)
+            if (progress.size < sequence.size) {
+                progress.clear()
+            }
+        }
+    }
+    fun checkSequence(gesture: Gesture) {
+        progress.add(element = gesture)
+        if (progress.takeLast(n = sequence.size) == sequence) {
+            onSequenceComplete()
+            progress.clear()
+        }
+    }
+
+
+    this
+        .nestedScroll(connection = nestedScrollConnection)
+        .pointerInput(key1 = Unit) {
+            coroutineScope {
+                launch {
+                    detectTapGestures {
+                        checkSequence(gesture = Gesture.TAP)
+                    }
+                }
+                launch {
+                    var dragStart: Offset = Offset.Zero
+                    var totalDrag: Offset = Offset.Zero
+                    detectDragGestures(
+                        onDragStart = { dragStart = it },
+                        onDrag = { change, dragAmount ->
+                            change.consume()
+                            totalDrag = change.position - dragStart
+                        },
+                        onDragEnd = {
+                            println("Total Drag (px): x=${totalDrag.x}, y=${totalDrag.y}")
+                            val gesture = onDragGestures(offset = totalDrag)
+                            println("GESTURE" + gesture)
+                            if (progress.lastOrNull() != gesture) checkSequence(gesture = gesture ?: return@detectDragGestures)
+                            dragStart = Offset.Zero
+                            totalDrag = Offset.Zero
+                        },
+                    )
+                }
+            }
+        }
+}
+
+private fun onDragGestures(offset: Offset): Gesture? {
+    val vertical = abs(x = offset.y)
+    val horizontal = abs(x = offset.x)
+    val leniency = 2.0
+    return when {
+        vertical > horizontal * leniency -> {
+            if (offset.y > 0) Gesture.SWIPE_DOWN else Gesture.SWIPE_UP
+        }
+
+        horizontal > vertical * leniency -> {
+            if (offset.x > 0) Gesture.SWIPE_RIGHT else Gesture.SWIPE_LEFT
+        }
+
+        else -> null
+    }
+}
+
+internal enum class Gesture {
+    SWIPE_UP,
+    SWIPE_DOWN,
+    SWIPE_LEFT,
+    SWIPE_RIGHT,
+    TAP
+}
