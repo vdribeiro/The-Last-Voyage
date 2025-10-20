0c95952b190ffe576d6bfe699e224434f6183e3e
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-10-20 09:50:24 +0100

    Easter Eggs

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/theme/component/modifier/GestureListener.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/theme/component/modifier/GestureListener.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
