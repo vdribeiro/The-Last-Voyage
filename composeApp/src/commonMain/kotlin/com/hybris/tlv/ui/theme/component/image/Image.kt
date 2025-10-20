d7e79bd145fd0b10c5860d50cb811746c5ee581b
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-10-20 14:06:21 +0100

    Image preview

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/theme/component/image/Image.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/theme/component/image/Image.kt
@@ -1,12 +1,15 @@
 package com.hybris.tlv.ui.theme.component.image
 
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import coil3.compose.AsyncImage
 import com.hybris.tlv.ui.theme.AppTheme
 import thelastvoyage.composeapp.generated.resources.Res
+import thelastvoyage.composeapp.generated.resources.ic_launcher_foreground
 
 @Composable
 internal fun Image(
@@ -15,9 +18,24 @@ internal fun Image(
     contentDescription: String? = null,
     contentScale: ContentScale = ContentScale.Crop,
 ) {
-    AsyncImage(
+    Image(
         modifier = modifier,
         model = runCatching { path?.let { Res.getUri(path = "drawable/$it") } }.getOrNull(),
+        contentDescription = contentDescription,
+        contentScale = contentScale,
+    )
+}
+
+@Composable
+internal fun Image(
+    modifier: Modifier = Modifier,
+    model: Any? = null,
+    contentDescription: String? = null,
+    contentScale: ContentScale = ContentScale.Crop,
+) {
+    AsyncImage(
+        modifier = modifier,
+        model = model,
         contentDescription = contentDescription,
         contentScale = contentScale,
     )
@@ -26,5 +44,10 @@ internal fun Image(
 @Preview
 @Composable
 private fun ImagePreview() = AppTheme {
-    Image(path = "ic_launcher_foreground.png")
+    Image(
+        model = Image(
+            painter = painterResource(Res.drawable.ic_launcher_foreground),
+            contentDescription = null,
+        )
+    )
 }
