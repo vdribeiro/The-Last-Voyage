9bef58b26877fd1762610f1f4101f2c309ff6d53
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-11-15 01:27:44 +0000

    runCatching improvements

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/theme/component/image/Image.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/theme/component/image/Image.kt
@@ -3,10 +3,13 @@ package com.hybris.tlv.ui.theme.component.image
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalInspectionMode
 import coil3.compose.AsyncImage
 import com.hybris.tlv.telemetry.Telemetry
 import com.hybris.tlv.ui.theme.AppTheme
@@ -20,18 +23,25 @@ internal fun Image(
     contentDescription: String? = null,
     contentScale: ContentScale = ContentScale.Crop,
 ) {
-    val model = runCatching {
-        image?.path?.let { Res.getUri(path = "drawable/$it") }
-    }.onFailure { Telemetry.error(tag = TAG, message = "Unable to get path", throwable = it) }.getOrNull() ?: runCatching {
-        image?.drawable?.let { painterResource(resource = it) }
-    }.onFailure { Telemetry.error(tag = TAG, message = "Unable to get painter", throwable = it) }.getOrNull()
-    if (model != null) AsyncImage(
-        modifier = modifier,
-        model = model,
-        contentDescription = contentDescription,
-        contentScale = contentScale,
-        onError = { Telemetry.error(tag = TAG, message = "Unable to draw image", throwable = it.result.throwable) }
-    ) else Box(modifier = modifier)
+    if (!LocalInspectionMode.current) {
+        val model = getUri(image) ?: getPainter(image) ?: image?.path
+        if (model == null) Box(modifier = modifier) else AsyncImage(
+            modifier = modifier,
+            model = model,
+            contentDescription = contentDescription,
+            contentScale = contentScale,
+            onError = { Telemetry.error(tag = TAG, message = "Unable to draw image", throwable = it.result.throwable) },
+            error = painterResource(resource = Res.drawable.ic_launcher_foreground)
+        )
+    } else {
+        val painter = getPainter(image)
+        if (painter == null) Box(modifier = modifier) else Image(
+            modifier = modifier,
+            painter = painter,
+            contentDescription = contentDescription,
+            contentScale = contentScale
+        )
+    }
 }
 
 internal data class ImageResource(
@@ -39,15 +49,18 @@ internal data class ImageResource(
     val drawable: DrawableResource? = null
 )
 
-@Preview
+private fun getUri(image: ImageResource?): String? = runCatching {
+    image?.path?.let { Res.getUri(path = "drawable/$it") }
+}.onFailure { Telemetry.error(tag = TAG, message = "Unable to get path", throwable = it) }.getOrNull()
+
 @Composable
-private fun ImagePathPreview() = AppTheme {
-    Image(image = ImageResource(path = "terrestrial_planet.jpg"))
-}
+private fun getPainter(image: ImageResource?): Painter? = runCatching {
+    image?.drawable?.let { painterResource(resource = it) }
+}.onFailure { Telemetry.error(tag = TAG, message = "Unable to get painter", throwable = it) }.getOrNull()
 
 @Preview
 @Composable
-private fun ImageDrawablePreview() = AppTheme {
+private fun ImagePreview() = AppTheme {
     Image(image = ImageResource(drawable = Res.drawable.ic_launcher_foreground))
 }
 
