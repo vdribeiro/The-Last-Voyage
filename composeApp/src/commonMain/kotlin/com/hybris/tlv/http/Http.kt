49718da3d07fca5155f06fa55a8014737fa36e37
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-11-23 22:16:56 +0000

    Added internet check debounce

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/http/Http.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/http/Http.kt
@@ -1,13 +1,15 @@
 package com.hybris.tlv.http
 
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.INFINITE
 import kotlin.time.Duration.Companion.ZERO
 import kotlin.time.Duration.Companion.seconds
+import kotlin.time.TimeMark
+import kotlin.time.TimeSource
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.prepareGet
 import io.ktor.client.statement.bodyAsChannel
@@ -15,9 +17,6 @@ import io.ktor.http.encodeURLPath
 import io.ktor.http.isSuccess
 import io.ktor.utils.io.toByteArray
 import com.hybris.tlv.flow.Dispatcher
-import com.hybris.tlv.locale.distantPast
-import com.hybris.tlv.locale.hasTimePassed
-import com.hybris.tlv.locale.now
 import com.hybris.tlv.platform.isDebug
 import com.hybris.tlv.serializer.decode
 import com.hybris.tlv.telemetry.Telemetry
@@ -33,7 +32,10 @@ internal suspend inline fun <reified T> HttpClient.getStream(
             queryMap.forEach { url.encodedParameters.append(name = it.key, value = it.value) }
             block()
         }.execute { httpResponse ->
-            val list: List<T> = httpResponse.body()
+            if (!httpResponse.status.isSuccess()) throw Throwable(message = "Unsuccessful response: ${httpResponse.status}")
+            val channel = httpResponse.bodyAsChannel()
+            val bytes = channel.toByteArray()
+            val list = decode<List<T>>(value = bytes.decodeToString()) ?: throw Throwable("Unable to decode response")
             Result.Success(list = list)
         }
     }
@@ -41,9 +43,11 @@ internal suspend inline fun <reified T> HttpClient.getStream(
 
 private suspend fun isInternetAvailableDebounced(): Boolean = runCatching {
     mutex.withLock {
-        if (!hasTimePassed(dateTime = lastCheckTime, duration = cacheTTL)) return@withLock lastKnownStatus
+        val now = timeSource.markNow()
+        val previous = lastCheckTime?.elapsedNow() ?: INFINITE
+        if (previous < cacheTTL) return@withLock lastKnownStatus
         lastKnownStatus = isInternetAvailable()
-        lastCheckTime = now()
+        lastCheckTime = now
         return@withLock lastKnownStatus
     }
 }.onFailure { Telemetry.error(tag = TAG, message = "Unable to check connectivity", throwable = it) }.getOrDefault(defaultValue = false)
@@ -52,5 +56,6 @@ private const val TAG = "Network"
 
 private val mutex = Mutex()
 private val cacheTTL: Duration = if (isDebug) ZERO else 5.seconds
-private var lastCheckTime = distantPast()
+private val timeSource = TimeSource.Monotonic
+private var lastCheckTime: TimeMark? = null
 private var lastKnownStatus = false
