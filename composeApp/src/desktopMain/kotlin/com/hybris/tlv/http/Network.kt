a4a92281e7a33a8db2e8b7afc70ae74ac121b4ab
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-11-23 16:36:46 +0000

    Desktop network check

--- composeApp/src/desktopMain/kotlin/com/hybris/tlv/http/Network.kt
+++ composeApp/src/desktopMain/kotlin/com/hybris/tlv/http/Network.kt
@@ -1,23 +1,12 @@
 package com.hybris.tlv.http
 
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.request.head
+import java.net.InetSocketAddress
+import java.net.Socket
 import com.hybris.tlv.telemetry.Telemetry
 
 internal actual suspend fun isInternetAvailable(): Boolean = runCatching {
-    val response = httpClient.head(urlString = "https://clients3.google.com/generate_204")
-    return response.status.value in 200..299
+    Socket().use { it.connect(InetSocketAddress("8.8.8.8", 53), 1500) }
+    true
 }.onFailure { Telemetry.error(tag = TAG, message = "Unable to check connectivity", throwable = it) }.getOrDefault(defaultValue = false)
-
-private val httpClient by lazy {
-    HttpClient {
-        install(plugin = HttpTimeout) {
-            connectTimeoutMillis = 1500L
-            socketTimeoutMillis = 1500L
-            requestTimeoutMillis = 1500L
-        }
-    }
-}
 
 private const val TAG = "Network"
