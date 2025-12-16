9ae6d862213a27590835cf488d44490e0f85ae09
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-12-16 17:03:19 +0000

    Removed icon

--- composeApp/src/desktopMain/kotlin/com/hybris/tlv/main.kt
+++ composeApp/src/desktopMain/kotlin/com/hybris/tlv/main.kt
@@ -1,6 +1,5 @@
 package com.hybris.tlv
 
-import org.jetbrains.compose.resources.painterResource
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.window.Window
@@ -12,8 +11,6 @@ import com.hybris.tlv.cheats.rememberKeySequenceCheats
 import com.hybris.tlv.telemetry.Telemetry
 import com.hybris.tlv.ui.theme.getTranslation
 import javafx.embed.swing.JFXPanel
-import thelastvoyage.composeapp.generated.resources.Res
-import thelastvoyage.composeapp.generated.resources.ic_launcher_round
 
 private const val TAG = "App"
 
@@ -30,15 +27,11 @@ fun main() = application {
     Telemetry.info(tag = TAG, message = "App started\nJavaFX = $initializeJfx")
 
     val appNameTranslation = getTranslation(key = "app_name")
-    val icon = runCatching {
-        painterResource(resource = Res.drawable.ic_launcher_round)
-    }.onFailure { Telemetry.error(tag = TAG, message = "Unable to get app icon", throwable = it) }.getOrNull()
     val windowState = rememberWindowState(placement = WindowPlacement.Maximized)
     val keyListener = rememberKeySequenceCheats()
 
     Window(
         title = appNameTranslation,
-        icon = icon,
         state = windowState,
         onPreviewKeyEvent = keyListener,
         onCloseRequest = ::exitApplication,
