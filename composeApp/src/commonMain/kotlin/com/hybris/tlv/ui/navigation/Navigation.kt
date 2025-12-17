e5d80920a038754856ca277c75df180b2d955010
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-12-17 23:16:39 +0000

    Improve Navigation commands

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/navigation/Navigation.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/navigation/Navigation.kt
@@ -1,23 +1,12 @@
 package com.hybris.tlv.ui.navigation
 
-import kotlin.reflect.KType
-import kotlin.reflect.typeOf
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
-import androidx.savedstate.SavedState
-import androidx.savedstate.read
-import androidx.savedstate.write
 import com.hybris.tlv.config.ConfigManager
-import com.hybris.tlv.serializer.decode
-import com.hybris.tlv.serializer.decodeURL
-import com.hybris.tlv.serializer.encode
-import com.hybris.tlv.serializer.encodeURL
-import com.hybris.tlv.telemetry.Telemetry
 import com.hybris.tlv.ui.navigation.graph.achievementScreen
 import com.hybris.tlv.ui.navigation.graph.cheatScreen
 import com.hybris.tlv.ui.navigation.graph.creditScreen
@@ -71,48 +60,3 @@ internal fun Navigation(
         creditScreen(useCases = useCases)
     }
 }
-
-/**
- * Navigate to the given [screen].
- * If the screen is not in the stack, add it to end of the stack.
- * If the screen is already in the stack, eiter replace or restore it and truncate onwards.
- */
-internal inline fun <reified S: Screen> NavHostController.navigate(screen: S, restore: Boolean) {
-    Telemetry.info(tag = TAG, message = "Navigating to: $screen")
-    navigate(route = screen) {
-        popUpTo(route = S::class) { inclusive = !restore }
-        launchSingleTop = restore
-    }
-    Telemetry.info(tag = TAG, message = "Navigation stack: ${printBackStack()}")
-}
-
-/**
- * Prints the current navigation back stack in a reader-friendly format.
- */
-private fun NavHostController.printBackStack() = runCatching {
-    currentBackStack.value.joinToString { it.destination.toString().substringAfterLast(delimiter = ".") }.substringAfter(delimiter = ",").trim()
-}.onFailure { Telemetry.error(tag = TAG, message = "Error printing backstack", throwable = it) }.getOrDefault(defaultValue = "")
-
-/**
- * Creates a map of destination arguments with a NavType for a serializable object of type [T].
- */
-internal inline fun <reified T> typeMapOf(): Map<KType, NavType<T?>> =
-    mapOf(pair = typeOf<T?>() to serializableType<T?>())
-
-private inline fun <reified T> serializableType(): NavType<T> =
-    object: NavType<T>(isNullableAllowed = true) {
-        override fun put(bundle: SavedState, key: String, value: T) {
-            encode(value = value)?.let { bundle.write { putString(key = key, value = it) } }
-        }
-
-        override fun get(bundle: SavedState, key: String): T? =
-            bundle.read { getStringOrNull(key = key)?.let { decode(value = it) } }
-
-        override fun serializeAsValue(value: T): String =
-            encodeURL(value = value)
-
-        override fun parseValue(value: String): T =
-            decodeURL<T>(value = value) as T
-    }
-
-private const val TAG = "Navigation"
---
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/navigation/Routing.kt
@@ -0,0 +1,59 @@
+package com.hybris.tlv.ui.navigation
+
+import kotlin.reflect.KType
+import kotlin.reflect.typeOf
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.savedstate.SavedState
+import androidx.savedstate.read
+import androidx.savedstate.write
+import com.hybris.tlv.serializer.decode
+import com.hybris.tlv.serializer.decodeURL
+import com.hybris.tlv.serializer.encode
+import com.hybris.tlv.serializer.encodeURL
+import com.hybris.tlv.telemetry.Telemetry
+
+/**
+ * Navigate to the given [screen].
+ * If the screen is not in the stack, add it to end of the stack.
+ * If the screen is already in the stack, eiter replace or restore it and truncate onwards.
+ */
+internal inline fun <reified S: Screen> NavHostController.navigate(screen: S, restore: Boolean) {
+    Telemetry.info(tag = TAG, message = "Navigating to: $screen")
+    navigate(route = screen) {
+        popUpTo(route = S::class) { inclusive = !restore }
+        launchSingleTop = restore
+    }
+    Telemetry.info(tag = TAG, message = "Navigation stack: ${printBackStack()}")
+}
+
+/**
+ * Prints the current navigation back stack in a reader-friendly format.
+ */
+private fun NavHostController.printBackStack() = runCatching {
+    currentBackStack.value.joinToString { it.destination.toString().substringAfterLast(delimiter = ".") }.substringAfter(delimiter = ",").trim()
+}.onFailure { Telemetry.error(tag = TAG, message = "Error printing backstack", throwable = it) }.getOrDefault(defaultValue = "")
+
+/**
+ * Creates a map of destination arguments with a NavType for a serializable object of type [T].
+ */
+internal inline fun <reified T> typeMapOf(): Map<KType, NavType<T?>> =
+    mapOf(pair = typeOf<T?>() to serializableType<T?>())
+
+private inline fun <reified T> serializableType(): NavType<T> =
+    object: NavType<T>(isNullableAllowed = true) {
+        override fun put(bundle: SavedState, key: String, value: T) {
+            encode(value = value)?.let { bundle.write { putString(key = key, value = it) } }
+        }
+
+        override fun get(bundle: SavedState, key: String): T? =
+            bundle.read { getStringOrNull(key = key)?.let { decode(value = it) } }
+
+        override fun serializeAsValue(value: T): String =
+            encodeURL(value = value)
+
+        override fun parseValue(value: String): T =
+            decodeURL<T>(value = value) as T
+    }
+
+private const val TAG = "Routing"
