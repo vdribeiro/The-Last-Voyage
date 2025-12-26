aa57dda60357f2d59feb5cf148cefb3af5dd5be6
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-12-26 22:49:51 +0000

    Tests

--- composeApp/src/commonTest/kotlin/com/hybris/tlv/screen/score/ScoreStoreTest.kt
+++ composeApp/src/commonTest/kotlin/com/hybris/tlv/screen/score/ScoreStoreTest.kt
@@ -1,34 +1,38 @@
 package com.hybris.tlv.screen.score
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import com.hybris.tlv.TestCase
+import com.hybris.tlv.gameSessionPrototype
+import com.hybris.tlv.navigation.Screen
 
-// TODO
 internal class ScoreStoreTest: TestCase() {
 
-//    private val store: ScoreStore get() = getScoreStore()
-//
-//    @BeforeTest
-//    fun setup() = runBlocking {
-//        reset()
-//        getNavigation().navigate(navigationState = NavigationState(screen = SplashScreen))
-//        getNavigation().navigate(navigationState = NavigationState(screen = MainMenuScreen))
-//        getNavigation().navigate(navigationState = NavigationState(screen = ScoreScreen))
-//    }
-//
-//    @Test
-//    fun `init`() = runBlocking {
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
-//        val latestGameSession = useCases.gameSession.getLatestGameSession()!!
-//        useCases.gameSession.updateGameSession(gameSession = latestGameSession.copy(score = 9000.0))
-//        val scoreStore = store
-//        assertEquals(expected = listOf(useCases.gameSession.getLatestGameSession()), actual = scoreStore.stateFlow.value.gameSessions)
-//    }
-//
-//    @Test
-//    fun `send action back`() = runBlocking {
-//        store
-//        assertEquals(expected = ScoreScreen, actual = getNavigation().stateFlow.value.screen)
-//        getNavigation().back()
-//        assertEquals(expected = MainMenuScreen, actual = getNavigation().stateFlow.value.screen)
-//    }
+    @Test
+    fun init() = runUnitTest {
+        useCases.ship.prepopulateEngines()
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        val latestGameSession = useCases.gameSession.getLatestGameSession()!!
+        useCases.gameSession.updateGameSession(gameSession = latestGameSession.copy(score = 9000.0))
+        val store = storeFactory.getScoreStore()
+        assertFalse(actual = store.state.loading)
+        assertEquals(expected = listOf(element = useCases.gameSession.getLatestGameSession()), actual = store.state.gameSessions)
+    }
+
+    @Test
+    fun initWithoutScores() = runUnitTest {
+        val store = storeFactory.getScoreStore()
+        assertFalse(actual = store.state.loading)
+        assertEquals(expected = emptyList(), actual = store.state.gameSessions)
+    }
+
+    @Test
+    fun navigateBack() = runUnitTest {
+        assertNavigationBackstack(list = emptyList())
+        navigate(screen = Screen.Score)
+        assertNavigationBackstack(list = listOf(element = Screen.Score))
+        storeFactory.getScoreStore().back()
+        assertNavigationBackstack(list = emptyList())
+    }
 }
