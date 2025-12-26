5341a1c918efc813046b9dcc03dbc64c067be055
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-12-26 16:30:06 +0000

    Tests

--- composeApp/src/commonTest/kotlin/com/hybris/tlv/screen/game/GameStoreTest.kt
+++ composeApp/src/commonTest/kotlin/com/hybris/tlv/screen/game/GameStoreTest.kt
@@ -1,150 +1,163 @@
 package com.hybris.tlv.screen.game
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import com.hybris.tlv.TestCase
+import com.hybris.tlv.gameSessionPrototype
+import com.hybris.tlv.navigation.Screen
+import com.hybris.tlv.planets
+import com.hybris.tlv.stellarHosts
 
-// TODO
 internal class GameStoreTest: TestCase() {
 
-//    private val store: GameStore get() = getGameStore()
-//
-//    @BeforeTest
-//    fun setup() = runBlocking {
-//        reset()
-//        getNavigation().navigate(navigationState = NavigationState(screen = GameScreen))
-//    }
-//
-//    @Test
-//    fun `init`() = runBlocking {
-//        useCases.space.syncStellarHosts()
-//        useCases.space.syncPlanets()
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
-//        val gameStore = store
-//        assertNotNull(actual = gameStore.stateFlow.value.ship)
-//        assertEquals(expected = Content.SYSTEM, actual = gameStore.stateFlow.value.currentContent)
-//        assertEquals(expected = stellarHosts.first(), actual = gameStore.stateFlow.value.currentStellarHost)
-//        assertEquals(
-//            expected = hostsWithPlanets.drop(n = 1).sortedBy { it.id },
-//            actual = gameStore.stateFlow.value.nearStellarHosts.sortedBy { it.id }
-//        )
-//    }
-//
-//    @Test
-//    fun `init without game session`() = runBlocking {
-//        assertEquals(expected = GameScreen, actual = getNavigation().stateFlow.value.screen)
-//        val gameStore = store
-//        assertNull(actual = gameStore.stateFlow.value.ship)
-//        assertEquals(expected = Screen.Feedback, actual = getNavigation().stateFlow.value.screen)
-//    }
-//
-//    @Test
-//    fun `ship is repaired`() = runBlocking {
-//        useCases.space.syncStellarHosts()
-//        useCases.space.syncPlanets()
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
-//        val gameSession = useCases.gameSession.getLatestGameSession()!!
-//        useCases.gameSession.updateGameSession(gameSession = gameSession.copy(ship = gameSession.ship.copy(integrity = 0)))
-//        val gameStore = store
-//        assertEquals(expected = 1, actual = gameStore.stateFlow.value.ship?.integrity)
-//        assertEquals(expected = 89, actual = gameStore.stateFlow.value.ship?.materials)
-//    }
-//
-//    @Test
-//    fun `game over by integrity`() = runBlocking {
-//        assertEquals(expected = GameScreen, actual = getNavigation().stateFlow.value.screen)
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
-//        val gameSession = useCases.gameSession.getLatestGameSession()!!
-//        useCases.gameSession.updateGameSession(gameSession = gameSession.copy(ship = gameSession.ship.copy(integrity = 0, materials = 0)))
-//        store
-//        assertEquals(expected = GameOverScreen, actual = getNavigation().stateFlow.value.screen)
-//    }
-//
-//    @Test
-//    fun `game over by fuel`() = runBlocking {
-//        assertEquals(expected = GameScreen, actual = getNavigation().stateFlow.value.screen)
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype.copy(ship = gameSessionPrototype.ship.copy(fuel = 0)))
-//        store
-//        assertEquals(expected = GameOverScreen, actual = getNavigation().stateFlow.value.screen)
-//    }
-//
-//    @Test
-//    fun `init without stellar host`() = runBlocking {
-//        assertEquals(expected = GameScreen, actual = getNavigation().stateFlow.value.screen)
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
-//        val gameStore = store
-//        assertNull(actual = gameStore.stateFlow.value.ship)
-//        assertEquals(expected = Screen.Feedback, actual = getNavigation().stateFlow.value.screen)
-//    }
-//
-//    @Test
-//    fun `send action back`() = runBlocking {
-//        useCases.space.syncStellarHosts()
-//        useCases.space.syncPlanets()
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
-//        store
-//        assertEquals(expected = GameScreen, actual = getNavigation().stateFlow.value.screen)
-//        getNavigation().back()
-//        assertEquals(expected = MainMenuScreen, actual = getNavigation().stateFlow.value.screen)
-//    }
-//
-//    @Test
-//    fun `send action change tab`() = runBlocking {
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
-//        val gameStore = store
-//
-//        gameStore.send(action = GameAction.ChangeTab(Content.SYSTEM))
-//        assertEquals(expected = Content.SYSTEM, actual = gameStore.stateFlow.value.currentContent)
-//
-//        gameStore.send(action = GameAction.ChangeTab(Content.SHIP))
-//        assertEquals(expected = Content.SHIP, actual = gameStore.stateFlow.value.currentContent)
-//
-//        gameStore.send(action = GameAction.ChangeTab(Content.TRAVEL))
-//        assertEquals(expected = Content.TRAVEL, actual = gameStore.stateFlow.value.currentContent)
-//    }
-//
-//    @Test
-//    fun `send action travel`() = runBlocking {
-//        useCases.space.syncStellarHosts()
-//        useCases.space.syncPlanets()
-//        assertEquals(expected = GameScreen, actual = getNavigation().stateFlow.value.screen)
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
-//        val gameStore = store
-//        gameStore.send(action = GameAction.Travel(stellarHost = stellarHosts[1]))
-//        assertEquals(expected = EventScreen, actual = getNavigation().stateFlow.value.screen)
-//    }
-//
-//    @Test
-//    fun `send action travel without game session`() = runBlocking {
-//        assertEquals(expected = GameScreen, actual = getNavigation().stateFlow.value.screen)
-//        val gameStore = store
-//        gameStore.send(action = GameAction.Travel(stellarHost = stellarHosts.first()))
-//        assertEquals(expected = Screen.Feedback, actual = getNavigation().stateFlow.value.screen)
-//    }
-//
-//    @Test
-//    fun `send action travel without stellar host`() = runBlocking {
-//        assertEquals(expected = GameScreen, actual = getNavigation().stateFlow.value.screen)
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
-//        val gameStore = store
-//        gameStore.send(action = GameAction.Travel(stellarHost = stellarHosts.first()))
-//        assertEquals(expected = Screen.Feedback, actual = getNavigation().stateFlow.value.screen)
-//    }
-//
-//    @Test
-//    fun `send action settle`() = runBlocking {
-//        useCases.space.syncStellarHosts()
-//        useCases.space.syncPlanets()
-//        assertEquals(expected = GameScreen, actual = getNavigation().stateFlow.value.screen)
-//        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
-//        val gameStore = store
-//        gameStore.send(action = GameAction.Settle(planet = planets.first()))
-//        assertEquals(expected = GameOverScreen, actual = getNavigation().stateFlow.value.screen)
-//    }
-//
-//    @Test
-//    fun `send action settle without game session`() = runBlocking {
-//        assertEquals(expected = GameScreen, actual = getNavigation().stateFlow.value.screen)
-//        val gameStore = store
-//        gameStore.send(action = GameAction.Settle(planet = planets.first()))
-//        assertEquals(expected = Screen.Feedback, actual = getNavigation().stateFlow.value.screen)
-//    }
+    @Test
+    fun init() = runUnitTest {
+        useCases.space.prepopulateStellarHosts()
+        useCases.space.prepopulatePlanets()
+        useCases.ship.prepopulateEngines()
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        val store = storeFactory.getGameStore(ship = null)
+        assertNotNull(actual = store.gameSession)
+        assertNotNull(actual = store.state.ship)
+        assertEquals(expected = Content.SYSTEM, actual = store.state.currentContent)
+        assertEquals(expected = stellarHosts.first(), actual = store.state.currentStellarHost)
+    }
+
+    @Test
+    fun initWithoutGameSession() = runUnitTest {
+        assertNavigationBackstack(list = emptyList())
+        storeFactory.getGameStore(ship = null)
+        assertNavigationBackstack(list = listOf(element = Screen.Feedback()))
+    }
+
+    @Test
+    fun initWithoutStellarHost() = runUnitTest {
+        assertNavigationBackstack(list = emptyList())
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        storeFactory.getGameStore(ship = null)
+        assertNavigationBackstack(list = listOf(element = Screen.Feedback()))
+    }
+
+    @Test
+    fun shipIsRepaired() = runUnitTest {
+        useCases.space.prepopulateStellarHosts()
+        useCases.space.prepopulatePlanets()
+        useCases.ship.prepopulateEngines()
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        val gameSession = useCases.gameSession.getLatestGameSession()!!
+        useCases.gameSession.updateGameSession(gameSession = gameSession.copy(ship = gameSession.ship.copy(integrity = 0)))
+        val store = storeFactory.getGameStore(ship = null)
+        assertEquals(expected = 1, actual = store.state.ship?.integrity)
+        assertEquals(expected = 89, actual = store.state.ship?.materials)
+    }
+
+    @Test
+    fun gameOverByIntegrity() = runUnitTest {
+        useCases.space.prepopulateStellarHosts()
+        useCases.space.prepopulatePlanets()
+        useCases.ship.prepopulateEngines()
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        val gameSession = useCases.gameSession.getLatestGameSession()!!
+        useCases.gameSession.updateGameSession(gameSession = gameSession.copy(ship = gameSession.ship.copy(integrity = 0, materials = 0)))
+        assertNavigationBackstack(list = emptyList())
+        storeFactory.getGameStore(ship = null)
+        assertNavigationBackstack(list = listOf(element = Screen.GameOver))
+
+    }
+
+    @Test
+    fun gameOverByFuel() = runUnitTest {
+        useCases.space.prepopulateStellarHosts()
+        useCases.space.prepopulatePlanets()
+        useCases.ship.prepopulateEngines()
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        val gameSession = useCases.gameSession.getLatestGameSession()!!
+        useCases.gameSession.updateGameSession(gameSession = gameSession.copy(ship = gameSession.ship.copy(fuel = 0)))
+        assertNavigationBackstack(list = emptyList())
+        storeFactory.getGameStore(ship = null)
+        assertNavigationBackstack(list = listOf(element = Screen.GameOver))
+    }
+
+    @Test
+    fun changeTab() = runUnitTest {
+        useCases.space.prepopulateStellarHosts()
+        useCases.space.prepopulatePlanets()
+        useCases.ship.prepopulateEngines()
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        val store = storeFactory.getGameStore(ship = null)
+
+        store.send(action = GameAction.ChangeTab(Content.SYSTEM))
+        assertEquals(expected = Content.SYSTEM, actual = store.state.currentContent)
+
+        store.send(action = GameAction.ChangeTab(Content.SHIP))
+        assertEquals(expected = Content.SHIP, actual = store.state.currentContent)
+
+        store.send(action = GameAction.ChangeTab(Content.TRAVEL))
+        assertEquals(expected = Content.TRAVEL, actual = store.state.currentContent)
+    }
+
+    @Test
+    fun travel() = runUnitTest {
+        useCases.space.prepopulateStellarHosts()
+        useCases.space.prepopulatePlanets()
+        useCases.ship.prepopulateEngines()
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        val store = storeFactory.getGameStore(ship = null)
+
+        assertNavigationBackstack(list = emptyList())
+        store.send(action = GameAction.Travel(stellarHost = stellarHosts.first { it.id == "proxima_cen" }))
+        assertNavigationBackstack(list = listOf(element = Screen.Event()))
+    }
+
+    @Test
+    fun travelWithoutGameSession() = runUnitTest {
+        assertNavigationBackstack(list = emptyList())
+        val store = storeFactory.getGameStore(ship = null)
+        store.send(action = GameAction.Travel(stellarHost = stellarHosts.first { it.id == "proxima_cen" }))
+        assertNavigationBackstack(list = listOf(element = Screen.Feedback()))
+    }
+
+    @Test
+    fun travelWithoutStellarHost() = runUnitTest {
+        assertNavigationBackstack(list = emptyList())
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        val store = storeFactory.getGameStore(ship = null)
+        store.send(action = GameAction.Travel(stellarHost = stellarHosts.first { it.id == "proxima_cen" }))
+        assertNavigationBackstack(list = listOf(element = Screen.Feedback()))
+    }
+
+    @Test
+    fun settle() = runUnitTest {
+        useCases.space.prepopulateStellarHosts()
+        useCases.space.prepopulatePlanets()
+        useCases.ship.prepopulateEngines()
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        val store = storeFactory.getGameStore(ship = null)
+
+        assertNavigationBackstack(list = emptyList())
+        store.send(action = GameAction.Settle(planet = planets.first()))
+        assertNavigationBackstack(list = listOf(element = Screen.GameOver))
+    }
+
+    @Test
+    fun settleWithoutGameSession() = runUnitTest {
+        assertNavigationBackstack(list = emptyList())
+        val store = storeFactory.getGameStore(ship = null)
+        store.send(action = GameAction.Settle(planet = planets.first()))
+        assertNavigationBackstack(list = listOf(element = Screen.Feedback()))
+    }
+
+    @Test
+    fun navigateBack() = runUnitTest {
+        assertNavigationBackstack(list = emptyList())
+        navigate(screen = Screen.Game())
+        assertNavigationBackstack(list = listOf(element = Screen.Game()))
+        useCases.space.prepopulateStellarHosts()
+        useCases.space.prepopulatePlanets()
+        useCases.ship.prepopulateEngines()
+        useCases.gameSession.startGame(gameSessionPrototype = gameSessionPrototype)
+        storeFactory.getGameStore(ship = null).back()
+        assertNavigationBackstack(list = listOf(Screen.Game(), Screen.MainMenu))
+    }
 }
