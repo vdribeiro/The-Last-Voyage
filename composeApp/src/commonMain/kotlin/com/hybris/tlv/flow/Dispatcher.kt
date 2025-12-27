63adebc28c86563b57257ba9612cf918b6cadaf8
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-12-27 14:49:24 +0000

    Tests

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/flow/Dispatcher.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/flow/Dispatcher.kt
@@ -12,7 +12,6 @@ import com.hybris.tlv.test.ShadowedInTesting
 
 /**
  * Dispatchers for coroutines.
- * The kotlin dispatchers are not directly used so that this custom implementation can be overridden in tests.
  * The [Main] dispatcher is used for UI-related operations.
  * The [Default] dispatcher is used for CPU-intensive operations.
  * The [IO] dispatcher is used for I/O-related operations.
