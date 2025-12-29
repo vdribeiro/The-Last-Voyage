fcbb7672389e4d8dd823417f507ff41b7ec35ae8
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-12-29 14:20:43 +0000

    Tests

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/test/Annotations.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/test/Annotations.kt
@@ -1,14 +1,14 @@
 package com.hybris.tlv.test
 
 /**
- * Indicates that this element should not be included in testing.
+ * Indicates that this file should not be included in testing.
  */
 @Retention(value = AnnotationRetention.BINARY)
 @Target(AnnotationTarget.FILE)
 internal annotation class ExcludeFromTesting
 
 /**
- * Indicates that this file must be shadowed in testing.
+ * Indicates that this file must be shadowed in testing, therefore it will also not be included in testing.
  */
 @Retention(value = AnnotationRetention.BINARY)
 @Target(AnnotationTarget.FILE)
