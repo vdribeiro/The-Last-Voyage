67c37ba43d2830834e4e80223a061c676c6011fb
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-12-31 10:58:32 +0000

    Tests

---
+++ composeApp/src/commonTest/kotlin/com/hybris/tlv/theme/component/bottombar/NavigationBarTest.kt
@@ -0,0 +1,57 @@
+package com.hybris.tlv.theme.component.bottombar
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotSelected
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.hybris.tlv.TestCase
+
+@OptIn(ExperimentalTestApi::class)
+internal class NavigationBarTest: TestCase() {
+
+    @Test
+    fun itemsAreDisplayed() = runUITest {
+        val items = listOf("Home", "Search", "Settings")
+        val selectedItem = "Search"
+        var capturedItem: String? = null
+
+        setScreen {
+            NavigationBar(
+                items = items,
+                selected = { it == selectedItem },
+                text = { it },
+                onClick = { capturedItem = it }
+            )
+        }
+
+        items.forEach { label -> onNodeWithText(text = label).assertIsDisplayed() }
+        onNodeWithText(text = "Home").assertIsNotSelected()
+        onNodeWithText(text = "Search").assertIsSelected()
+        onNodeWithText(text = "Settings").assertIsNotSelected()
+
+        onNodeWithText(text = "Home").performClick()
+        assertEquals(expected = "Home", actual = capturedItem)
+    }
+
+    @Test
+    fun disabled() = runUITest {
+        val items = listOf("Home")
+        var clickCount = 0
+
+        setScreen {
+            NavigationBar(
+                items = items,
+                enabled = { false },
+                text = { it },
+                onClick = { clickCount++ }
+            )
+        }
+
+        onNodeWithText(text = "Home").performClick()
+        assertEquals(expected = 0, actual = clickCount)
+    }
+}
