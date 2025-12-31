db2282e1fff1bf92b7b9b99b875c1068143a0875
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2025-12-31 14:27:06 +0000

    Audio player

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/audio/Player.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/audio/Player.kt
@@ -48,7 +48,6 @@ private fun getTracks(destination: NavDestination?): List<String>? = runCatching
             "tracks/blind_shift.mp3",
             "tracks/graduality.mp3",
             "tracks/led_twilight.mp3",
-            "tracks/my_very_own_dead_ship.mp3",
             "tracks/neon_sky.mp3",
             "tracks/rain_in_space.mp3",
             "tracks/space_gras.mp3",
