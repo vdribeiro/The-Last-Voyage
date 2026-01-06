745898100f603657bdbf79bf8b4db6020cbd31f0
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-01-06 10:28:21 +0000

    Network

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/http/HttpClientFactory.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/http/HttpClientFactory.kt
@@ -9,7 +9,9 @@ import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.compression.ContentEncoding
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.LoggingConfig
 import io.ktor.serialization.kotlinx.json.json
 import com.hybris.tlv.platform.isDebug
 import com.hybris.tlv.serializer.json
@@ -24,22 +26,20 @@ internal class HttpClientFactory(engine: HttpClientEngine?) {
      * Installs and configures the necessary plugins for the [HttpClient], namely logging, timeouts, caching, content negotiation, and compression.
      */
     private fun <T: HttpClientEngineConfig> HttpClientConfig<T>.install() {
-        install(plugin = Logging) {
-            logger = object: io.ktor.client.plugins.logging.Logger {
-                override fun log(message: String) {
-                    Telemetry.info(tag = "Ktor", message = message)
-                }
-            }
-            level = if (isDebug) LogLevel.ALL else LogLevel.INFO
-        }
-        install(plugin = HttpTimeout) {
-            connectTimeoutMillis = 10_000L
-            socketTimeoutMillis = 20_000L
-            requestTimeoutMillis = 60_000L
-        }
+        install(plugin = Logging) { configure() }
+        install(plugin = HttpTimeout)
         install(plugin = HttpCache)
         install(plugin = ContentNegotiation) { json(json = json) }
         install(plugin = ContentEncoding) { gzip(quality = 0.9F) }
+    }
+
+    private fun LoggingConfig.configure() {
+        logger = object: Logger {
+            override fun log(message: String) {
+                Telemetry.info(tag = TAG, message = message)
+            }
+        }
+        level = if (isDebug) LogLevel.ALL else LogLevel.INFO
     }
 
     /**
@@ -48,5 +48,10 @@ internal class HttpClientFactory(engine: HttpClientEngine?) {
     val httpClient: HttpClient = when (engine) {
         null -> HttpClient { install() }
         else -> HttpClient(engine = engine) { install() }
+    }
+
+    companion object {
+        private const val TAG = "HttpClient"
+        private const val MAX_RETRIES = 3
     }
 }
