e1c9f451d1e143b60dd556330eb74ea8a17d1d4b
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-01-20 14:22:51 +0000

    Previews

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/theme/Translations.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/theme/Translations.kt
@@ -48,5 +48,5 @@ internal fun RefreshTranslations(getTranslations: suspend () -> List<Translation
  */
 @Composable
 internal fun InjectTranslations(translations: List<Translation>) {
-    InjectTranslations(translations = translations)
+    TranslationCache.set(translations = translations)
 }
