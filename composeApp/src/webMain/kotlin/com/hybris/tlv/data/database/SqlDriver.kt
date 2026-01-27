55e9648c3bef85e3f4a8a18260e51fe399c5363a
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-01-27 13:40:21 +0000

    Web

--- composeApp/src/webMain/kotlin/com/hybris/tlv/data/database/SqlDriver.kt
+++ composeApp/src/webMain/kotlin/com/hybris/tlv/data/database/SqlDriver.kt
@@ -6,7 +6,6 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlSchema
 import app.cash.sqldelight.driver.worker.WebWorkerDriver
 import com.hybris.tlv.core.flow.Dispatcher
-import com.hybris.tlv.domain.flag.FeatureFlags.flags
 import org.w3c.dom.Worker
 
 @OptIn(ExperimentalWasmJsInterop::class)
@@ -15,27 +14,20 @@ internal actual suspend fun createSqlDriver(
     schema: SqlSchema<QueryResult.AsyncValue<Unit>>,
     inMemory: Boolean
 ): SqlDriver = withContext(context = Dispatcher.IO) {
-    val worker: Worker = if (flags.devMode) getDebugWorker() else getWorker()
-    WebWorkerDriver(worker = worker).also { driver ->
+    WebWorkerDriver(worker = getWorker()).also { driver ->
         schema.create(driver = driver).await()
     }
 }
 
 @OptIn(ExperimentalWasmJsInterop::class)
-private fun getDebugWorker(): Worker = js(
-    code = """
-        new Worker(
-            new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url)
-        )
-    """
-)
-
-@OptIn(ExperimentalWasmJsInterop::class)
 private fun getWorker(): Worker = js(
     code = """
-        new Worker(
-            'sqljs.worker.js', 
-            { type: 'module' }
-        )
+        (() => {
+            const workerPath = ['sqljs', 'worker.js'].join('.'); 
+            return new Worker(
+                new URL(workerPath, import.meta.url), 
+                { type: 'module' }
+            );
+        })()
     """
 )
--- composeApp/webpack.config.d/sqljs.js
+++ composeApp/webpack.config.d/sqljs.js
@@ -8,31 +8,17 @@ config.resolve.fallback = {
     crypto: false,
 };
 
-const isProduction = config.mode === 'production';
-if (isProduction) {
-    config.plugins.push(
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, "../../node_modules/@cashapp/sqldelight-sqljs-worker/sqljs.worker.js"),
-                    to: "sqljs.worker.js"
-                },
-                {
-                    from: path.resolve(__dirname, "../../node_modules/sql.js/dist/sql-wasm.wasm"),
-                    to: "sql-wasm.wasm"
-                }
-            ]
-        })
-    );
-} else {
-    config.plugins.push(
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, "../../node_modules/sql.js/dist/sql-wasm.wasm"),
-                    to: "."
-                }
-            ]
-        })
-    );
-}
+config.plugins.push(
+    new CopyWebpackPlugin({
+        patterns: [
+            {
+                from: path.resolve(__dirname, "../../node_modules/@cashapp/sqldelight-sqljs-worker/sqljs.worker.js"),
+                to: "sqljs.worker.js"
+            },
+            {
+                from: path.resolve(__dirname, "../../node_modules/sql.js/dist/sql-wasm.wasm"),
+                to: "sql-wasm.wasm"
+            }
+        ]
+    })
+);
