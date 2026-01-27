04aa4615ff29f0484b57018ef158722163dbe604
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-01-27 17:40:16 +0000

    Web

--- composeApp/webpack.config.d/sqljs.js
+++ composeApp/webpack.config.d/sqljs.js
@@ -8,29 +8,12 @@ config.resolve.fallback = {
     crypto: false,
 };
 
-config.resolve.alias = {
-    ...config.resolve.alias,
-    'sqljs.worker.js': path.resolve(__dirname, "../../node_modules/@cashapp/sqldelight-sqljs-worker/sqljs.worker.js")
-};
-
-config.module.rules.push({
-    test: /sqljs\.worker\.js$/,
-    type: 'asset/resource',
-    generator: {
-        filename: 'sqljs.worker.js'
-    }
-});
-
 config.plugins.push(
     new CopyWebpackPlugin({
         patterns: [
             {
-                from: path.resolve(__dirname, "../../node_modules/@cashapp/sqldelight-sqljs-worker/sqljs.worker.js"),
-                to: "sqljs.worker.js"
-            },
-            {
                 from: path.resolve(__dirname, "../../node_modules/sql.js/dist/sql-wasm.wasm"),
-                to: "sql-wasm.wasm"
+                to: "."
             }
         ]
     })
