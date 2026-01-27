07b6a3fa33732c8c51d55a35a2c30c71d0576fba
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-01-27 13:58:39 +0000

    Web

--- composeApp/src/webMain/kotlin/com/hybris/tlv/data/database/SqlDriver.kt
+++ composeApp/src/webMain/kotlin/com/hybris/tlv/data/database/SqlDriver.kt
@@ -6,6 +6,7 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlSchema
 import app.cash.sqldelight.driver.worker.WebWorkerDriver
 import com.hybris.tlv.core.flow.Dispatcher
+import com.hybris.tlv.domain.flag.FeatureFlags.flags
 import org.w3c.dom.Worker
 
 @OptIn(ExperimentalWasmJsInterop::class)
@@ -14,16 +15,29 @@ internal actual suspend fun createSqlDriver(
     schema: SqlSchema<QueryResult.AsyncValue<Unit>>,
     inMemory: Boolean
 ): SqlDriver = withContext(context = Dispatcher.IO) {
-    WebWorkerDriver(worker = getWorker()).also { driver ->
+    val worker: Worker = if (flags.devMode) getDebugWorker() else getWorker()
+    WebWorkerDriver(worker = worker).also { driver ->
         schema.create(driver = driver).await()
     }
 }
 
 @OptIn(ExperimentalWasmJsInterop::class)
+private fun getDebugWorker(): Worker = js(
+    code = """
+        new Worker(
+            new URL(
+                "@cashapp/sqldelight-sqljs-worker/sqljs.worker.js",
+                 import.meta.url
+             )
+        )
+    """
+)
+
+@OptIn(ExperimentalWasmJsInterop::class)
 private fun getWorker(): Worker = js(
     code = """
         new Worker(
-            new URL('sqljs.worker.js', document.baseURI).href, 
+            'sqljs.worker.js', 
             { type: 'module' }
         )
     """
