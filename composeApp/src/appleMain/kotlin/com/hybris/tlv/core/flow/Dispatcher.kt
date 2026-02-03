51f521228204464c8359394a3fa8ea83f512962b
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-02-03 15:25:56 +0000

    Sentry

--- composeApp/src/appleMain/kotlin/com/hybris/tlv/core/flow/Dispatcher.kt
+++ composeApp/src/appleMain/kotlin/com/hybris/tlv/core/flow/Dispatcher.kt
@@ -2,5 +2,6 @@ package com.hybris.tlv.core.flow
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 
 internal actual val io: CoroutineDispatcher = Dispatchers.IO
