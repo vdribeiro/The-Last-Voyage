3f3e3d3300e7e22d590b6947d5eee257c8d795d3
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-02-20 14:59:47 +0000

    Added catastrophe and event Explorer

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/screen/eventexplorer/Filters.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/screen/eventexplorer/Filters.kt
@@ -1,15 +1,16 @@
 package com.hybris.tlv.ui.screen.eventexplorer
 
 import com.hybris.tlv.domain.usecase.event.model.Event
+import com.hybris.tlv.domain.usecase.translation.TranslationCache
 
 internal fun List<Event>.search(search: String): List<Event> =
     if (search.isNotBlank()) {
         val searchLowercase = search.lowercase()
         filter { event ->
             listOfNotNull(
-                event.id,
-                event.description,
-                event.parentId,
+                TranslationCache.get(key = event.id),
+                TranslationCache.get(key = event.description),
+                event.parentId?.let { TranslationCache.get(key = it) }
             ).any { it.lowercase().contains(other = searchLowercase) }
         }
     } else this
