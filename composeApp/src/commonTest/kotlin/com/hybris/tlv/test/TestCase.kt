b1a7eb5b34247d45dfdc42e5b5d699774288a620
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-03-10 10:34:23 +0000

    App

--- composeApp/src/commonTest/kotlin/com/hybris/tlv/test/TestCase.kt
+++ composeApp/src/commonTest/kotlin/com/hybris/tlv/test/TestCase.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.test.setMain
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ProvidedValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.test.ComposeUiTest
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.runComposeUiTest
@@ -182,10 +183,12 @@ internal abstract class TestCase: PlatformTestCase() {
         content: @Composable () -> Unit
     ) {
         setContent {
-            val providers = buildList {
-                addAll(elements = compositionValues)
-                add(element = LocalLifecycleOwner provides lifecycleOwner)
-            }.toTypedArray()
+            val providers = remember {
+                buildList {
+                    addAll(elements = compositionValues)
+                    add(element = LocalLifecycleOwner provides lifecycleOwner)
+                }.toTypedArray()
+            }
 
             CompositionLocalProvider(values = providers) {
                 AppTheme {
