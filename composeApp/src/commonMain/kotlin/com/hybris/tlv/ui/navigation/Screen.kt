c0d04b6cd3f837d6f995208f733a4ce533ba6d5b
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-03-10 21:54:01 +0000

    Screens

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/navigation/Screen.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/ui/navigation/Screen.kt
@@ -12,7 +12,7 @@ import com.hybris.tlv.domain.usecase.ship.model.Ship
 @Serializable
 internal sealed interface Screen {
     @Serializable
-    data class Splash(val reset: Boolean): Screen
+    data class Splash(val reset: Boolean = false): Screen
     @Serializable
     data object Cheat: Screen
     @Serializable
@@ -20,17 +20,17 @@ internal sealed interface Screen {
     @Serializable
     data object Help: Screen
     @Serializable
-    data class Feedback(val tag: String?, val message: String?): Screen
+    data class Feedback(val tag: String? = null, val message: String? = null): Screen
     @Serializable
     data object NewGame: Screen
     @Serializable
-    data class Tutorial(val newGame: Boolean): Screen
+    data class Tutorial(val newGame: Boolean = false): Screen
     @Serializable
     data object Catastrophe: Screen
     @Serializable
-    data class Game(val ship: Ship?): Screen
+    data class Game(val ship: Ship? = null): Screen
     @Serializable
-    data class Event(val ship: Ship?): Screen
+    data class Event(val ship: Ship? = null): Screen
     @Serializable
     data object GameOver: Screen
     @Serializable
