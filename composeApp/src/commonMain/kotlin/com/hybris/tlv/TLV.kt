aa8302fb021a7f522c49527befce5bdfcfb5fffb
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-03-19 19:40:20 +0000

    Translations

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/TLV.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/TLV.kt
@@ -11,17 +11,18 @@ import com.hybris.tlv.core.audio.createAudioPlayer
 import com.hybris.tlv.core.flow.Dispatcher
 import com.hybris.tlv.core.locale.observeLocale
 import com.hybris.tlv.core.platform.isDebug
-import com.hybris.tlv.data.resource.JsonResource
 import com.hybris.tlv.core.telemetry.Logger
 import com.hybris.tlv.core.telemetry.Telemetry
 import com.hybris.tlv.data.database.NoOpSqlDriver
 import com.hybris.tlv.data.database.createSqlDriver
 import com.hybris.tlv.data.http.NoOpHttpEngine
 import com.hybris.tlv.data.http.createHttpEngine
+import com.hybris.tlv.data.resource.JsonResource
 import com.hybris.tlv.data.serializer.loadFromJsonResource
 import com.hybris.tlv.domain.flag.FeatureFlags
 import com.hybris.tlv.domain.flag.Flags
 import com.hybris.tlv.domain.usecase.translation.TranslationCache
+import com.hybris.tlv.domain.usecase.translation.TranslationUseCases
 import com.hybris.tlv.domain.usecase.translation.model.Translation
 import com.hybris.tlv.test.ExcludeFromTesting
 
@@ -65,12 +66,7 @@ internal object TLV {
             this@TLV.dependency.update { dependency }
 
             Telemetry.info(tag = TAG, message = "Registering locale listener")
-            observeLocale().collectLatest { languageIso ->
-                val translations = dependency.useCases.translation.getTranslations(languageIso = languageIso).ifEmpty {
-                    loadFromJsonResource(json = JsonResource.Translations)
-                }
-                TranslationCache.set(translations = translations)
-            }
+            observeLocale(translation = dependency.useCases.translation)
         }
     }
 
@@ -105,5 +101,17 @@ internal object TLV {
         }.onFailure {
             Telemetry.error(tag = TAG, message = "Unable to create the Dependency Index", throwable = it)
         }.getOrNull()
+    }
+
+    /**
+     * Observe and register the locale listener.
+     */
+    private suspend fun observeLocale(translation: TranslationUseCases) {
+        observeLocale().collectLatest { languageIso ->
+            val translations = translation.getTranslations(languageIso = languageIso).ifEmpty {
+                loadFromJsonResource(json = JsonResource.Translations)
+            }
+            TranslationCache.set(translations = translations)
+        }
     }
 }
