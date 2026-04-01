717442fdc1bd87e9fb7b89c1d603d474395ca68d
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-04-01 16:08:06 +0100

    Logger

--- composeApp/src/androidMain/kotlin/com/hybris/tlv/core/telemetry/PlatformLogger.kt
+++ composeApp/src/androidMain/kotlin/com/hybris/tlv/core/telemetry/PlatformLogger.kt
@@ -5,10 +5,24 @@ import android.util.Log
 internal actual object PlatformLogger {
 
     actual fun info(tag: String, message: String) {
-        Log.i(tag, message)
+        log(message = message) { Log.i(tag, it) }
     }
 
     actual fun error(tag: String, message: String, throwable: Throwable?) {
-        Log.e(tag, message, throwable)
+        log(message = "$message${throwable?.let { "\n${it.stackTraceToString()}" }.orEmpty()}") { Log.e(tag, it) }
     }
+
+    /**
+     * Log in chunks to work around the 4KB buffer limit of the Logcat.
+     */
+    private fun log(message: String, log: (String) -> Unit) {
+        if (message.length <= CHUNK_SIZE) log(message)
+        else message.chunked(CHUNK_SIZE).forEachIndexed { index, chunk -> log("($index) $chunk") }
+    }
+
+    /**
+     * Chunk size for log messages.
+     * It assumes an average of 2-bytes per character.
+     */
+    private const val CHUNK_SIZE = 2000
 }
