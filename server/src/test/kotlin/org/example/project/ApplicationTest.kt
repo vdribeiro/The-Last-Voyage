551452c70c8a952a5a47c4746bf5fbf4bc45dad2
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-04-18 00:02:59 +0100

    Server

--- server/src/test/kotlin/org/example/project/ApplicationTest.kt
+++ server/src/test/kotlin/org/example/project/ApplicationTest.kt
@@ -2,6 +2,7 @@ package org.example.project
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlinx.coroutines.test.TestResult
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
@@ -11,12 +12,12 @@ import com.hybris.tlv.module
 class ApplicationTest {
 
     @Test
-    fun testRoot() = testApplication {
+    fun testRoot(): TestResult = testApplication {
         application {
             module()
         }
-        val response = client.get("/")
-        assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals("Ktor Server", response.bodyAsText())
+        val response = client.get(urlString = "/")
+        assertEquals(expected = HttpStatusCode.OK, actual = response.status)
+        assertEquals(expected = "Ktor Server", actual = response.bodyAsText())
     }
 }
