85f3cbcc6b662e16b0d4ec7471d3d92b11138a04
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-04-18 21:26:11 +0100

    Core split

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,40 +1,118 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.kotlin.multiplatform.library)
-    alias(libs.plugins.android.lint)
+    alias(notation = libs.plugins.kotlin.multiplatform)
+    alias(notation = libs.plugins.android.kotlin.multiplatform.library)
+    alias(notation = libs.plugins.android.lint)
 }
 
+//region Properties
+val appId: String = "com.hybris.tlv.core"
+val appFramework = "TLV"
+val appVersion: String = "1.2.0"
+
+val jdkVersion = 21
+val jvmVersion = JvmTarget.JVM_21
+
+val androidSdkTarget: IntRange = 26..36
+//endregion
+
 kotlin {
+    jvmToolchain(jdkVersion = jdkVersion)
+
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xexpect-actual-classes")
+    }
 
     android {
-        namespace = "com.hybris.tlv.core"
-        compileSdk {
-            version = release(36) {
-                minorApiLevel = 1
+        namespace = appId
+        minSdk = androidSdkTarget.first
+        compileSdk = androidSdkTarget.last
+    }
+
+    val iosTargets = listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).apply {
+        forEach { iosTarget ->
+            iosTarget.binaries.framework {
+                baseName = appFramework
+                isStatic = true
+                version = appVersion
+                freeCompilerArgs += "-Xbinary=bundleId=$appId"
             }
         }
-        minSdk = 26
+    }
+
+    jvm(name = "desktop") {
+        compilerOptions {
+            jvmTarget.set(jvmVersion)
+        }
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    val webTarget = wasmJs {
+        outputModuleName = appFramework
+        browser {
+            commonWebpackConfig {
+                outputFileName = "tlv.js"
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static(directory = "build/processedResources/wasmJs/main")
+                }
+                showProgress = true
+                cssSupport {
+                    enabled.set(true)
+                }
+            }
+        }
+        binaries.executable()
     }
 
     sourceSets {
-        commonMain {
+        val commonMain by getting {
             dependencies {
-                implementation(libs.kotlin.stdlib)
-                implementation(libs.bundles.common)
+                implementation(dependencyNotation = libs.kotlin.stdlib)
+                implementation(dependencyNotation = libs.bundles.common)
             }
         }
 
-        commonTest {
+        val commonTest by getting {
             dependencies {
-                implementation(libs.kotlin.test)
+                implementation(dependencyNotation = libs.bundles.common.test)
             }
         }
 
-        androidMain {
+        getByName("androidMain") {
             dependencies {
-                implementation(libs.bundles.android)
+                implementation(dependencyNotation = libs.bundles.android)
             }
         }
+
+        val appleMain by creating {
+            dependsOn(other = commonMain)
+            dependencies {
+                implementation(dependencyNotation = libs.bundles.ios)
+            }
+        }
+        iosTargets.forEach { iosTarget ->
+            sourceSets.getByName("${iosTarget.name}Main").dependsOn(other = appleMain)
+        }
+
+        getByName("desktopMain") {
+            dependencies {
+                implementation(dependencyNotation = libs.bundles.desktop)
+            }
+        }
+
+        val webMain by creating {
+            dependsOn(commonMain)
+            dependencies {
+                implementation(dependencyNotation = libs.bundles.web)
+            }
+        }
+        sourceSets.getByName("${webTarget.name}Main").dependsOn(other = webMain)
     }
-
 }
