cd9f12d9012a25a67f6805714d46820ef85fc333
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-04-20 11:09:24 +0100

    System

--- composeApp/src/desktopMain/kotlin/com/hybris/tlv/core/system/System.kt
+++
@@ -1,7 +0,0 @@
-package com.hybris.tlv.core.system
-
-internal actual val isDebug: Boolean by lazy {
-    runCatching {
-        System.getProperty("debug") == "true"
-    }.getOrDefault(defaultValue = false)
-}
