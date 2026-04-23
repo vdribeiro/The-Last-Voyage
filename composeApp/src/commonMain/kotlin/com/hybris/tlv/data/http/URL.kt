db72e0a10467ec39aacd6fdbc1f2c8b41f1ddbdc
Vitor Ribeiro <vdribeiro.vr@gmail.com>
2026-04-23 11:01:24 +0100

    Http

--- composeApp/src/commonMain/kotlin/com/hybris/tlv/data/http/URL.kt
+++ composeApp/src/commonMain/kotlin/com/hybris/tlv/data/http/URL.kt
@@ -1,6 +1,7 @@
 package com.hybris.tlv.data.http
 
 import com.hybris.tlv.core.platform.Platform
+import com.hybris.tlv.core.platform.isDebug
 import com.hybris.tlv.core.platform.platform
 
 /**
@@ -10,19 +11,19 @@ import com.hybris.tlv.core.platform.platform
  */
 internal sealed class URL(val path: String) {
     data object ExoplanetArchive: URL(path = "$EXOPLANETS_BASE_URL/TAP/sync")
-    data object Configs: URL(path = "$BASE_URL/configs.json")
-    data object Translations: URL(path = "$BASE_URL/translations.json")
-    data object Catastrophes: URL(path = "$BASE_URL/catastrophes.json")
-    data object CatastrophesTranslations: URL(path = "$BASE_URL/catastrophes_translations.json")
-    data object Engines: URL(path = "$BASE_URL/engines.json")
-    data object EnginesTranslations: URL(path = "$BASE_URL/engines_translations.json")
-    data object StellarHosts: URL(path = "$BASE_URL/hosts.json")
-    data object Planets: URL(path = "$BASE_URL/planets.json")
-    data object Events: URL(path = "$BASE_URL/events.json")
-    data object EventsTranslations: URL(path = "$BASE_URL/events_translations.json")
-    data object Achievements: URL(path = "$BASE_URL/achievements.json")
-    data object AchievementsTranslations: URL(path = "$BASE_URL/achievements_translations.json")
-    data object Credits: URL(path = "$BASE_URL/credits.json")
+    data object Configs: URL(path = "$baseUrl/configs.json")
+    data object Translations: URL(path = "$baseUrl/translations.json")
+    data object Catastrophes: URL(path = "$baseUrl/catastrophes.json")
+    data object CatastrophesTranslations: URL(path = "$baseUrl/catastrophes_translations.json")
+    data object Engines: URL(path = "$baseUrl/engines.json")
+    data object EnginesTranslations: URL(path = "$baseUrl/engines_translations.json")
+    data object StellarHosts: URL(path = "$baseUrl/hosts.json")
+    data object Planets: URL(path = "$baseUrl/planets.json")
+    data object Events: URL(path = "$baseUrl/events.json")
+    data object EventsTranslations: URL(path = "$baseUrl/events_translations.json")
+    data object Achievements: URL(path = "$baseUrl/achievements.json")
+    data object AchievementsTranslations: URL(path = "$baseUrl/achievements_translations.json")
+    data object Credits: URL(path = "$baseUrl/credits.json")
 
     companion object {
         const val EXOPLANETS_BASE_URL = "https://exoplanetarchive.ipac.caltech.edu"
@@ -31,5 +32,6 @@ internal sealed class URL(val path: String) {
             Platform.Android -> "http://10.0.2.2:8080/data"
             else -> "http://localhost:8080/data"
         }
+        val baseUrl: String get() = if (isDebug) devBaseUrl else BASE_URL
     }
 }
